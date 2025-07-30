263fd882201bd53ad03ddab9414cce9ba5e4968e
Dongmyoung Min <zmarzmzm@naver.com>
2025-07-31 00:05:21 +0900

    :wrench:Settings: 초기 세팅

---
+++ src/test/java/com/likelion/picklbe/PicklbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.picklbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicklbeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
