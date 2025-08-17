9eb657930a09dbad8fcc68821656aca1a9cbde64
이채민 <chaemin@ichaemin-ui-MacBookAir.local>
2025-08-17 22:48:47 +0900

    ✨ Feat: 퀴즈 식재료 누락 아이콘 추가

--- src/main/resources/db/migration/R__seed_ingredient_icons.sql
+++ src/main/resources/db/migration/R__seed_ingredient_icons.sql
@@ -189,3 +189,178 @@ WHERE name = '장류';
 UPDATE ingredient
 SET icon_url = '🧂'
 WHERE name = '쌀가루';
+
+-- 해제 대상 4종(미역/대파/다시마/곶감)은 퀴즈에서 제외하지만, 아이콘은 임시값으로 채워둠
+UPDATE ingredient
+SET icon_url = '🌿'
+WHERE name = '대파';
+UPDATE ingredient
+SET icon_url = '🫘'
+WHERE name = '두부';
+UPDATE ingredient
+SET icon_url = '🍙'
+WHERE name = '김';
+UPDATE ingredient
+SET icon_url = '🪸'
+WHERE name = '미역';
+UPDATE ingredient
+SET icon_url = '🥫'
+WHERE name = '된장';
+UPDATE ingredient
+SET icon_url = '🪸'
+WHERE name = '다시마';
+UPDATE ingredient
+SET icon_url = '🍊'
+WHERE name = '곶감';
+UPDATE ingredient
+SET icon_url = '🥕'
+WHERE name = '당근';
+
+UPDATE ingredient
+SET icon_url = '🥩'
+WHERE name = '소고기';
+UPDATE ingredient
+SET icon_url = '🦐'
+WHERE name = '새우';
+UPDATE ingredient
+SET icon_url = '🦑'
+WHERE name = '오징어';
+UPDATE ingredient
+SET icon_url = '🐙'
+WHERE name = '문어';
+UPDATE ingredient
+SET icon_url = '🦪'
+WHERE name = '굴';
+UPDATE ingredient
+SET icon_url = '🐚'
+WHERE name = '조개';
+
+UPDATE ingredient
+SET icon_url = '🥚'
+WHERE name = '계란';
+UPDATE ingredient
+SET icon_url = '🫐'
+WHERE name = '블루베리';
+UPDATE ingredient
+SET icon_url = '🍊'
+WHERE name = '오렌지';
+UPDATE ingredient
+SET icon_url = '🍇'
+WHERE name = '포도';
+UPDATE ingredient
+SET icon_url = '🍐'
+WHERE name = '배';
+UPDATE ingredient
+SET icon_url = '🍑'
+WHERE name = '복숭아';
+UPDATE ingredient
+SET icon_url = '🍓'
+WHERE name = '딸기';
+UPDATE ingredient
+SET icon_url = '🥭'
+WHERE name = '망고';
+UPDATE ingredient
+SET icon_url = '🥑'
+WHERE name = '아보카도';
+
+UPDATE ingredient
+SET icon_url = '🫒'
+WHERE name = '올리브오일';
+UPDATE ingredient
+SET icon_url = '🫙'
+WHERE name = '카놀라유';
+UPDATE ingredient
+SET icon_url = '🌻'
+WHERE name = '해바라기유';
+UPDATE ingredient
+SET icon_url = '🫘'
+WHERE name = '들깨';
+
+UPDATE ingredient
+SET icon_url = '🌾'
+WHERE name = '귀리';
+UPDATE ingredient
+SET icon_url = '🌾'
+WHERE name = '보리';
+UPDATE ingredient
+SET icon_url = '🫘'
+WHERE name = '콩';
+UPDATE ingredient
+SET icon_url = '🫘'
+WHERE name = '병아리콩';
+UPDATE ingredient
+SET icon_url = '🌾'
+WHERE name = '율무';
+
+UPDATE ingredient
+SET icon_url = '🥜'
+WHERE name = '브라질너트';
+UPDATE ingredient
+SET icon_url = '🥜'
+WHERE name = '피스타치오';
+UPDATE ingredient
+SET icon_url = '🥜'
+WHERE name = '호두';
+UPDATE ingredient
+SET icon_url = '🥜'
+WHERE name = '캐슈넛';
+
+UPDATE ingredient
+SET icon_url = '🫘'
+WHERE name = '참깨';
+UPDATE ingredient
+SET icon_url = '🫘'
+WHERE name = '치아씨드';
+UPDATE ingredient
+SET icon_url = '🫘'
+WHERE name = '아마씨';
+UPDATE ingredient
+SET icon_url = '🍫'
+WHERE name = '카카오닙스';
+
+UPDATE ingredient
+SET icon_url = '🥫'
+WHERE name = '고추장';
+UPDATE ingredient
+SET icon_url = '🫙'
+WHERE name = '간장';
+UPDATE ingredient
+SET icon_url = '🧂'
+WHERE name = '후추';
+UPDATE ingredient
+SET icon_url = '🌶️'
+WHERE name = '고춧가루';
+UPDATE ingredient
+SET icon_url = '🧂'
+WHERE name = '카레';
+UPDATE ingredient
+SET icon_url = '🧂'
+WHERE name = '계피';
+UPDATE ingredient
+SET icon_url = '🥬'
+WHERE name = '김치';
+
+UPDATE ingredient
+SET icon_url = '🍞'
+WHERE name = '빵';
+UPDATE ingredient
+SET icon_url = '☕'
+WHERE name = '커피';
+UPDATE ingredient
+SET icon_url = '🍵'
+WHERE name = '녹차';
+UPDATE ingredient
+SET icon_url = '🫖'
+WHERE name = '홍차';
+UPDATE ingredient
+SET icon_url = '💧'
+WHERE name = '물';
+UPDATE ingredient
+SET icon_url = '🥤'
+WHERE name = '탄산음료';
+UPDATE ingredient
+SET icon_url = '🥤'
+WHERE name = '에너지드링크';
+UPDATE ingredient
+SET icon_url = '🧃'
+WHERE name = '주스';
