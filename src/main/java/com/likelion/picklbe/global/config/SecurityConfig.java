b2eb97cceccd6514ad3e3f46b65c32dfdd4c2b65
Dongmyoung Min <zmarzmzm@naver.com>
2025-08-20 23:33:32 +0900

    :recycle:Refactor: 코드 리팩토링

--- src/main/java/com/likelion/picklbe/global/config/SecurityConfig.java
+++ src/main/java/com/likelion/picklbe/global/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.likelion.picklbe.global.config;
 
-import com.likelion.picklbe.global.security.JwtAuthenticationFilter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,6 +13,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.likelion.picklbe.global.security.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
