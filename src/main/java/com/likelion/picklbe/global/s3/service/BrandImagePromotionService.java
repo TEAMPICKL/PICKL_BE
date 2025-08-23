e9a24c3c92ffc77fe27915c4f9c671545899c63f
이채민 <chaemin@ichaemin-ui-MacBookAir.local>
2025-08-24 02:22:14 +0900

    🐛 Fix: martimage

---
+++ src/main/java/com/likelion/picklbe/global/s3/service/BrandImagePromotionService.java
@@ -0,0 +1,40 @@
+package com.likelion.picklbe.global.s3.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.likelion.picklbe.domain.brand.Brand;
+
+import com.amazonaws.services.s3.AmazonS3;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BrandImagePromotionService {
+
+  private final AmazonS3 s3;
+
+  @Value("${cloud.aws.s3.bucket}")
+  private String bucket;
+
+  @Value("${cloud.aws.s3.path.brand:images/brand}")
+  private String brandPath;
+
+  public String promote(String uuidKey, Brand brand) {
+    if (brand == null || brand.filename() == null) {
+      throw new IllegalArgumentException("고정 파일명이 없는 브랜드입니다: " + brand);
+    }
+    String srcKey = trimSlash(brandPath) + "/" + trimSlash(uuidKey);
+    String dstKey = trimSlash(brandPath) + "/" + brand.filename();
+
+    s3.copyObject(bucket, srcKey, bucket, dstKey);
+    s3.deleteObject(bucket, srcKey);
+
+    return dstKey;
+  }
+
+  private String trimSlash(String s) {
+    return s == null ? "" : s.replaceAll("^/+", "").replaceAll("/+$", "");
+  }
+}
